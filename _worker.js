63275b27f88b315e028bba486bc2f9a47d667062
ui-beam-9 <yui_daisuki@163.com>
2025-03-27 22:10:03 +0800

    repair

--- _worker.js
+++ _worker.js
@@ -1,8 +1,9 @@
 export default {
     async fetch(request, env) {
       let url = new URL(request.url);
-      if (url.pathname.startsWith('/')) {
-        url.hostname = 'https://pub-8a38c9993bf84440ad2a326d442b4635.r2.dev'//替换为你的存储桶给出的域名
+      const path = url.pathname.replace(/^\//, '');
+      if (path) {
+        url.hostname = 'pub-8a38c9993bf84440ad2a326d442b4635.r2.dev';
         let new_request = new Request(url, request);
         return fetch(new_request);
       }
